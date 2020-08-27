4f4e438e069ae365bb3d375606fb8f07f4ba0ba1
zaien24 <zaien24@gmail.com>
2020-08-27 18:13:43 +0900

    [Jdbctest] [DataSourceTest] [의존성 주입] [mybatis 연동] [HikariCp 커넥션 풀]

--- src/main/java/org/zeorck/sample/SampleHotel.java
+++ src/main/java/org/zeorck/sample/SampleHotel.java
@@ -3,19 +3,26 @@ package org.zeorck.sample;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Component
 @ToString
 @Getter
+// @AllArgsConstructor
+@RequiredArgsConstructor
 public class SampleHotel {
 	
+	@NonNull
 	private Chef chef;
 	
-	public SampleHotel(Chef chef) {
-		
-		this.chef = chef;
-				
-	}
+	/*
+	 * public SampleHotel(Chef chef) {
+	 * 
+	 * this.chef = chef;
+	 * 
+	 * }
+	 */
 
 }
